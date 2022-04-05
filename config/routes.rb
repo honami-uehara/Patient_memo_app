77b9dea1677bce644ddb0082a6951de65c8dc6f0
honami-uehara <honaa_cos.627i@icloud.com>
2022-04-06 00:57:56 +0900

    ログイン画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'users/index', to: 'users/registrations#index'
+    get 'users/destroy', to: 'users/sessions#destroy'
+  end
+
+  root 'home#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
