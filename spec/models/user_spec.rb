12565f606d5d77f935160c8ab732dec04d1c478b
honami-uehara <honaa_cos.627i@icloud.com>
2022-05-01 21:19:24 +0900

    モデルスペック追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
 
-  describe '#create' do
-    it '全ての項目の入力されれば登録できること' do
-      expect(create(:user)).to be_valid
+  describe 'バリデーション' do
+    context '登録できる' do
+      let(:username) { '123456789' }
+        it '全ての項目の入力されている' do
+          expect(create(:user)).to be_valid
+        end
+
+        it '名前が１０文字以下' do
+          expect(user.valid?).to eq(true)
+        end
+    end
+  end
+
+  context '登録できない' do
+    it '名前欄が空白' do
+      user.username = ''
+      expect(user.valid?).to eq(false)
     end
 
-    it 'バリデーションがかけれているか確認' do
-      user = build(:user)
-      user.valid?
-      expect(user).to be_valid
+    it 'メールアドレス欄が空白' do
+      user.email = ''
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'パスワード欄が空白' do
+      user.password = ''
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'パスワードと確認が一致していない' do
+      user.password, password_confirmation = ''
+      expect(user.valid?).to eq(false)
+    end
+
+    it '名前が１０文字以上だと登録できない' do
+      user.username = '12345678901'
+      expect(user.valid?).to eq(false)
     end
 
     it 'メールアドレスが重複していたら登録できないこと' do
