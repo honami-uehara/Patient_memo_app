e68f2c4875fda1e9dfa9adc82bede0bc9d642649
honami-uehara <honaa_cos.627i@icloud.com>
2022-06-09 22:42:01 +0900

    rspec作成中

--- spec/models/patient_registration_spec.rb
+++ spec/models/patient_registration_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe PatientRegistration, type: :model do
   let(:patient_registration) { create(:patient_registration) }
   let!(:user) { create(:user) }
-  # let!(:visit_date) { build(:post, visit_date: Date.today + 1) }
 
   describe 'バリデーション' do
     context '登録する' do
@@ -55,10 +54,12 @@ RSpec.describe PatientRegistration, type: :model do
     end
   end
 
-  # context "日付に関するバリデーション" do
-  #   it "来院日が今日より先の日付だと登録できないこと" do
-  #     expect(patient_registration.valid?).to be false
-  #     expect(patient_registration.errors[:visit_date]).to include("は今日以前の日付で登録してください")
-  #   end
-  # end
+   context "日付に関するバリデーション" do
+     let(:date) { build(:patient_registration, visit_date: "2023-05-02" ) }
+
+     it "来院日が今日より先の日付だと登録できないこと" do
+       expect(patient_registration.valid?).to eq(true)
+       expect(patient_registration.errors.messages[:visit_date]).to include("今日を含む過去の日付を入力して下さい")
+     end
+   end
 end
--- spec/requests/patient_registrations_spec.rb
+++ spec/requests/patient_registrations_spec.rb
@@ -1,4 +1,165 @@
 require 'rails_helper'
 
 RSpec.describe "PatientRegistrations", type: :request do
+  let(:patient_registration) { create(patient_registration, user_id: user.id) }
+  let(:user) { create(:user) }
+
+  describe 'GET/index' do
+    before do
+      sign_in user
+      get patient_registrations_path
+    end
+
+    it 'リクエストが成功すること' do
+      expect(response.status).to eq 200
+    end
+
+    it 'タイトルが表示されていること' do
+      expect(response.body).to include "登録者一覧"
+    end
+  end
+
+  describe 'GET/new' do
+    before do
+      sign_in user
+    end
+    it 'リクエストが成功すること' do
+      get new_patient_registration_path
+      expect(response.status).to eq 200
+    end
+  end
+
+
+  describe 'GET/show' do
+    let(:user) { create(:user) }
+    let(:patient_registration) { create(:patient_registration, user_id: user.id) }
+
+    before do
+      sign_in user
+      get patient_registration_path(patient_registration.id)
+    end
+
+    it 'リクエストが成功すること' do
+       expect(response.status).to eq 200
+    end
+
+    it '名前が表示できること' do
+      expect(response.body).to include patient_registration.name
+    end
+
+    it '電話番号が表示できること' do
+      expect(response.body).to include patient_registration.phone_number
+    end
+
+    it '注意することが表示できること' do
+      expect(response.body).to include patient_registration.warn
+    end
+
+    it '治療内容が表示できること' do
+      expect(response.body).to include patient_registration.maintenance_or_treatment
+    end
+
+    it '来院日が表示できること' do
+      expect(response.body).to include patient_registration.visit_date.to_s
+    end
+  end
+
+  describe 'GET/edit' do
+    let(:user) { create(:user) }
+    let(:patient_registration) { create(:patient_registration, user_id: user.id) }
+
+    before do
+      sign_in user
+      get edit_patient_registration_path(patient_registration.id)
+    end
+
+    it 'リクエストが成功すること' do
+       expect(response.status).to eq 200
+    end
+
+    it '名前が表示できること' do
+      expect(response.body).to include patient_registration.name
+    end
+
+    it '電話番号が表示できること' do
+      expect(response.body).to include patient_registration.phone_number
+    end
+
+    it '注意することが表示できること' do
+      expect(response.body).to include patient_registration.warn
+    end
+
+    it '治療内容が表示できること' do
+      expect(response.body).to include patient_registration.maintenance_or_treatment
+    end
+
+    it '来院日が表示できること' do
+      expect(response.body).to include patient_registration.visit_date.to_s
+    end
+  end
+
+  describe 'POST/create' do
+    context 'パラメーターが正常な場合' do
+      let(:params) { { patient_registration: attributes_for(:patient_registration) } }
+
+      before do
+        sign_in user
+      end
+
+      it 'データーベースへの保存が成功すること' do
+        expect do
+          post patient_registrations_path(params)
+        end.to change{ PatientRegistration.count }.by(1)
+       end
+
+      it 'リクエストが成功する' do
+        post patient_registrations_path, params: params
+        expect(response.status).to eq 302
+      end
+    end
+
+    context 'パラメーターが異常な場合' do
+      let(:invalid_params) { { patient_registration: attributes_for(:patient_registration) } }
+
+      it 'リクエストが成功する' do
+        post patient_registrations_path, params: invalid_params
+        expect(response.status).to eq 200
+      end
+
+      it 'データーベースが保存されない' do
+        expect do
+          post patient_registrations_path(invalid_params)
+        end.not_to change{ PatientRegistration.count }
+      end
+    end
+  end
+
+  describe 'POST/destory' do
+    before do
+      sign_in user
+    end
+   let(:patient_registration) { create(:patient_registration, user_id: user.id) }
+
+    it '削除されること' do
+      expect do
+        delete patient_registrations_path(patient_registration.id)
+      end.to change{ PatientRegistration.count }.by(-1)
+    end
+  end
+
+  # describe 'アクセス制限' do
+  #   context 'ログインページにリダイレクトされること' do
+  #     get patient_registrations_path
+  #     expect(response).to redirect_to(new_user_session_path)
+  #   end
+  # end
+
+  describe 'PATCH/update' do
+    before do
+      sign_in user
+    end
+    
+    # it 'データが更新されること' do
+    # end
+  end
 end
