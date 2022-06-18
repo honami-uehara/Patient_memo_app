69b1b4c1fe1c9089a5b3b15c3f7d81ceae1a87a7
honami-uehara <honaa_cos.627i@icloud.com>
2022-06-18 22:38:01 +0900

    rspec 修正

--- spec/requests/additional_comments_spec.rb
+++ spec/requests/additional_comments_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "AdditionalComments", type: :request do
   let(:user) { create(:user) }
   let(:patient_registration_record) { create(:patient_registration) }
+  let!(:params_2) { { patient_registration: attributes_for(:patient_registration) } }
 
 
   before do
@@ -10,10 +11,31 @@ RSpec.describe "AdditionalComments", type: :request do
   end
 
   describe 'Patient_registration/additionalcomment/create' do
+
     it 'responds successfully' do
-      post patient_registration_additional_comments_path(patient_registration_record.id)
+      post patient_registration_additional_comments_path, params: params_2
       expect(response.status).to eq 302
     end
+  end
+
+  it 'データーベースへの保存が成功すること' do
+    expect do
+      post patient_registration_additional_comments_path(params_2)
+    end.to change{ AdditionalComment.count }.by(1)
+  end
+
+  describe 'additionalcomment/destory' do
+    let!(:additional_comment) { create(:additional_comment, patient_registration: patient_registration_record, user: user) }
+
+    before do
+      sign_in user
+    end
+
+    it '削除されること' do
+     expect do
+       delete  patient_registration_additional_comment_path( patient_registration_record.id, additional_comment.id)
+     end.to change{ AdditionalComment.count }.by(-1)
+   end
   end
 end
 
