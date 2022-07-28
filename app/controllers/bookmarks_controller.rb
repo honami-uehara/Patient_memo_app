71df56509d212a95e849eef070ab07b20b35b75c
honami-uehara <honaa_cos.627i@icloud.com>
2022-07-28 11:19:14 +0000

    flash追加・編集

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,13 +2,24 @@
 
 class BookmarksController < ApplicationController
   def create
-    @bookmark = current_user.bookmarks.create(patient_id: params[:patient_id])
-    redirect_back(fallback_location: root_path)
+    bookmark = current_user.bookmarks.create(patient_id: params[:patient_id])
+    if bookmark.save
+      flash[:alert] = 'ブックマークしました'
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = 'ブックマーク失敗です'
+      render 'patients/show'
+    end
   end
 
   def destroy
-    @bookmark = Bookmark.find_by(patient_id: params[:patient_id], user_id: current_user.id)
-    @bookmark.destroy
-    redirect_back(fallback_location: root_path)
+    bookmark = Bookmark.find_by(patient_id: params[:patient_id], user_id: current_user.id)
+    if bookmark.destroy
+      flash[:alert] = 'ブックマーク取り消しました'
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = 'ブックマーク取り消し失敗です'
+      render 'patients/show'
+    end
   end
 end
