7f4e32eb9a76a10260eacd9338f539e8e7c14758
honami-uehara <honaa_cos.627i@icloud.com>
2022-08-09 12:56:37 +0000

    ゲストログイン修正

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -18,6 +18,8 @@ class Patient < ApplicationRecord
   validate :day_after_today, on: :create
 
   def day_after_today
-    errors.add(:visit_date, '今日を含む過去の日付を入力して下さい') if visit_date > Date.today
+    unless visit_date == nil
+      errors.add(:visit_date, '今日を含む過去の日付を入力して下さい') if visit_date > Date.today
+    end
   end
 end
